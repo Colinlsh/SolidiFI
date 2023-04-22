4eb9e72ba85ef19ab76bc3b278d1efcdcdca559f
Colinlsh <colinlsh6@gmail.com>
2023-04-22 22:28:06 +0800

    update * update setup file

--- setup.py
+++ setup.py
@@ -3,20 +3,11 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='solidifi',
-    version='0.0.2',
+    name="solidifi",
+    version="0.0.2",
     packages=find_packages(),
-    install_requires=[
-        'matplotlib',
-        'ijson',
-        'numpy',
-        'configparser',
-        'pandas'
-    ],
-    entry_points={
-            'console_scripts': [
-                'solidifi = solidifi.__main__:main'
-            ]
-    },
-    py_modules=['solidifi']
+    install_requires=["matplotlib", "ijson", "numpy", "configparser", "pandas"],
+    zip_safe=False,
+    entry_points={"console_scripts": ["solidifi = solidifi.__main__:main"]},
+    py_modules=["solidifi"],
 )
